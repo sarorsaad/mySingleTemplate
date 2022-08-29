2f0272c4e9fd3440abebe57fc7f68b52185e99fd
sarorsaad <sarorsaad@gmail.com>
2022-08-29 06:09:27 +0300

    html pages added

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-  path("", views.index)
+  path("", views.index),
+  path("function/", views.function),
+  path("navbar/", views.navbar),
+  path("calclusTXT/", views.calclusTXT),
+  path("linearAlgebra/", views.linearAlgebra),
+#   path("calclusTXT/", views.calclusTXT),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render  (request, "myapp/index.html")
+
+def function(request):
+  return render  (request, "myapp/function.html")
+
+def navbar(request):
+  return render  (request, "myapp/navbar.html")
+
+def calclusTXT(request):
+  return render  (request, "myapp/calclusTXT.html")
+def linearAlgebra(request):
+  return render  (request, "myapp/linearAlgebra.html")
+# def calclusTXT(request):
+#   return render  (request, "myapp/calclusTXT.html")
