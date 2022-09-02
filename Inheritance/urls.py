da7119680e87205ec3b631bcc4442326747d6fc9
sarorsaad <sarorsaad@gmail.com>
2022-09-02 06:01:27 +0300

    urls tag

--- Inheritance/urls.py
+++ Inheritance/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-  path("index/", views.index),
-  path("base/", views.base),
-  path("about/", views.about),
-  path("footer/", views.footer),
-  path("sidebar/", views.sidebar),
-  path("navbar/", views.navbar),
+  path("index/", views.index,name='index'),
+  path("base/", views.base,name='base'),
+  path("about/", views.about,name='about'),
+  path("footer/", views.footer,name='footer'),
+  path("sidebar/", views.sidebar,name='sidebar'),
+  path("navbar/", views.navbar,name='navbar'),
 ]
   
